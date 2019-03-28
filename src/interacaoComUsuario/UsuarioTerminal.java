b8f73a6cce2f44b2ed39c3a09357948e8fbb7cc9
wellitongb <wellitongb@lcc-05>
2019-03-28 18:45:23 -0300

    Mudanças realizadas por Wellinton e Thiago.

--- src/interacaoComUsuario/UsuarioTerminal.java
+++ src/interacaoComUsuario/UsuarioTerminal.java
@@ -9,73 +9,91 @@ import dados.ServicoException;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import servicos.UsuarioServico;
 
 /**
  * Interface da apresentação do usuário
  * @author SamDan
  */
 public class UsuarioTerminal {
+    
+    private UsuarioServico usuarioServico;
+    private int LoginErrado = 0; 
 
+    public UsuarioTerminal() {
+        this.usuarioServico = new UsuarioServico();
+    }
+    
+    
+    
 //    public abstract void realizarLogin(String login, String senha);
     public void apresentacao( ){
-        int key = 0;
+        int repetir = 0;
         Scanner entradaUsuario = new Scanner(System.in);
-        System.out.println("Bem vindo(a) ao LibraryManage! \n"
-                + "Escolha o número de uma das opções: \n"
-                + "(1)Realizar login \n"
-                + "(2)Buscar livro \n");
-        while(key == 0){
-            String entradaNumero = entradaUsuario.next();
-            if(entradaNumero.equals("1"))
-            {
-                key = 1;
-                System.out.println("\n");
-                System.out.println("REALIZANDO LOGIN -");
-                while(key == 1)
-                {
-                    System.out.print("Digite seu login:");
-                    String loginTemporario = entradaUsuario.next();
+        while(repetir == 0){
+            repetir = 1;
+            System.out.println("Bem vindo(a) ao LibraryManage! \n"
+                    + "Digite o número de uma das opções: \n"
+                    + "(1)Realizar login \n"
+                    + "(2)Buscar livro \n");
+            while(repetir == 1){
+                String entradaNumero = entradaUsuario.next();
+                if(entradaNumero.equals("1")){
+                    repetir = 2;
+                    System.out.println("");
+                    System.out.println("REALIZANDO LOGIN...");
+                    while(repetir == 2){
+                        System.out.print("Digite seu login:");
+                        String loginTemporario = entradaUsuario.next();
 
-                    System.out.print("Digite sua senha:");
-                    String senhaTemporaria = entradaUsuario.next();
-                    
-                    key = 2;
-                    
-                    try {
-                        this.autenticacao(loginTemporario, senhaTemporaria);
-                    } catch (ServicoException ex) {
-                        key = 1;
-                        System.err.println(ex.getMessage());
+                        System.out.print("Digite sua senha:");
+                        String senhaTemporaria = entradaUsuario.next();
+
+                        repetir = 3;
+
+                        try {
+                            this.autenticacao(loginTemporario, senhaTemporaria);
+                        } catch (ServicoException ex) {
+                            repetir = 2;
+                            System.err.println(ex.getMessage());
+                            if(ex.getMessage().equals("Login de usuario nao cadastrado")){
+                                this.LoginErrado++;
+                                if(this.LoginErrado < 3) {
+                                    System.out.println("Erro " + LoginErrado 
+                                            + " de 3. Com 3 erros você será redirecinado"
+                                            + "para o menu inicial.");
+                                }
+                                if(this.LoginErrado == 3){
+                                    this.LoginErrado = 0;
+                                    repetir = 0;
+                                }
+                            }                            
+                        }
                     }
+
+
                 }
-                
-                
-            }
-            else if(entradaNumero.equals("2"))
-            {
-                key = 1;
-                // coisas de buscar livro
-            }
-            else
-            {
-                 System.out.println("Escolha inválida! Tente 1 para REALIZAR LOGIN \n"
-                         + "ou 2 para BUSCAR LIVRO. \n");
+                else if(entradaNumero.equals("2")){
+                    repetir = 2;
+                    // coisas de buscar livro
+                }
+                else
+                {
+                     System.out.println("Escolha inválida! \n" 
+                             + "Digite o número de uma das opções: \n"
+                             + "(1)Realizar login \n"
+                             + "(2)Buscar livro \n");
+                }
             }
         }
-        
-        
     }
     
-    private void autenticacao(String login, String senha) 
-                                    throws ServicoException{
-    
-        
-        
-        
-        
+    private void autenticacao(String login, String senha) throws ServicoException{
+        this.usuarioServico.autenticacao(login, senha);
+    }
+    public void realizarLogout(){
         
     }
-    public void realizarLogout(){}
     
 }
 
